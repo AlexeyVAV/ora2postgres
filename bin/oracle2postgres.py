dbb072dfb0992185d97333255364cefbb5ab7d56
AlexeyVAV <vitkalovav@gmail.com>
2017-12-28 18:09:16 +0000

    2017-12-28 dh_work working RC

--- bin/oracle2postgres.py
+++ bin/oracle2postgres.py
@@ -3,14 +3,14 @@ import sys
 import re
 
 def param_load(sys_argv):
-    print("Load parameters")
+    print("Load parameters",sys_argv)
     if len(sys_argv) < 2:
         return "../etc/ora2psql_dt.map"
     else:
         return sys.argv[0]
 
 def mapping_load(mapping_file):
-    print("Load mapping file")
+    print("Load mapping file",mapping_file)
 
     try:
         with open(mapping_file) as _mf:
@@ -33,51 +33,54 @@ def source_ddl_load(ddl_file):
     print("Load source DDL file:", ddl_file)
 
 def search_in_line(line_in, mapping):
-    print("Search in line")
+    #print("Search in line")
+    line_s = re.split("\W+", line_in)
+    r1,r2 = 0,0
     for i in range(len(mapping)):
-        line = len([x for x in line_in.split() if x == mapping[i][0]])
-    ####
-    for i in range(len(mapping)):
-        #print(mapping[i][0])
-        #serch_line = re.compile("\b"+mapping[i][0]+"\b",re.I)
-        #if serch_line.serarch(line_in):
-        if re.findall(mapping[i][0], line_in):
-            print("Found",mapping[i][0],"in", line_in)
+        if mapping[i][0] in line_s:
+            #print("Found",mapping[i][0],"in", line_s,"replace with",mapping[i][1])
+            r1,r2 = mapping[i][0],mapping[i][1]
+            #return mapping[i][0], mapping[i][1]
+        #else:
+    return r1,r2
 
-def dt_replace(source_file, target_file):
+def dt_replace(source_file, target_file, ora_dt_map):
     #with open("../inbound/2017-12-20_uscomdv1_marketplace_tables.sql") as f:
-    ora_dt_map = mapping_load("../etc/ora2psql_dt.map")
+    #ora_dt_map = mapping_load("../etc/ora2psql_dt.map")
     with open(source_file) as f:
         for line in f:
-            #print(line)
-            # outLine = pattern.sub(r"VARCHAR", line)
-            search_in_line(line, ora_dt_map)
-            #if re.search("VARCHAR2", line):
-            #    outLine = re.sub(r"VARCHAR2", "VARCHAR", line, flags=re.IGNORECASE)
-            #    print (outLine, re.findall("VARCHAR[(](\d+)", outLine))
+            str1, str2 = search_in_line(line, ora_dt_map)
+            #print(str1,str2)
+            if str1 != 0:
+                if re.search(str1, line):
+                    outLine = re.sub(str1, str2, line, flags=re.IGNORECASE)
+                    #print (line,"to",outLine)
+                    print(outLine)
+                    #print (outLine, re.findall("VARCHAR[(](\d+)", outLine))
+            else:
+                print(line)
 
 
 ############################################################################################
 def main(sys_params):
-    print(sys_params)
+    #print(sys_params)
 
     # mapping_load(param_load(sys.argv))
     ora_dt_map = mapping_load("../etc/ora2psql_dt.map")
     if ora_dt_map > 0:
         print("Length: ",len(ora_dt_map))
-        #for i in range(len(ora_dt_map)):
-        #    print(ora_dt_map[i][0])
-        #print(ora_dt_map[1][0])
         print(type(ora_dt_map))
 
     #source_ddl_load("../inbound/ddl_script.sql")
-    dt_replace("../inbound/2017-12-20_uscomdv1_marketplace_tables.sql", "target_file")
+    dt_replace("../inbound/2017-12-20_uscomdv1_marketplace_tables.sql", "target_file", ora_dt_map)
+
+   # print
 
 # Main #
 if __name__ == "__main__":
     print("Main part")
-
-    if param_load(sys.argv) == 0:
+    params = param_load(sys.argv)
+    if params == 0:
         print("Error")
     else:
-        main(param_load(sys.argv))
+        main(params)
