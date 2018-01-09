c216165028f71d5e98e74b91c788be41c33ae925
AlexeyVAV <vitkalovav@gmail.com>
2018-01-09 19:13:15 +0000

    2018-01-09 DH replacement is working

--- bin/ora2postgre_v1.py
+++ bin/ora2postgre_v1.py
@@ -2,12 +2,14 @@ import os
 import sys
 import pandas as pd
 
+
 def param_load(sys_argv):
     print("Load parameters",sys_argv)
     if len(sys_argv) < 2:
         return "../etc/ora2psql_dt.map"
     else:
         return sys.argv[0]
+
 
 def mapping_load(mapping_file):
     print("Load mapping file",mapping_file)
@@ -18,7 +20,6 @@ def mapping_load(mapping_file):
     except:
         print("Error loading mapping file")
         return 0
-        raise
 
     try:
         o2p_map = [s.split("\t") for s in _o2p_map if len(s) > 0 and "#" not in s.lstrip()[0]]
@@ -26,9 +27,20 @@ def mapping_load(mapping_file):
     except IndexError:
         print("Mapping file error")
         return 0
-        raise
 
-def source_load(source_file):
+
+def sub_dt(line_in, mapping):
+    #print("Search in line")
+
+    res1 = " {} ({})".format(line_in[3],line_in[4])
+
+    for _i in range(len(mapping)):
+        if mapping[_i][0] == line_in[3]:
+            res1 = " {}({})".format(mapping[_i][1],line_in[4])
+    return res1
+
+
+def source_load(source_file,ora_dt_map):
 
     with open(source_file, 'r') as f:
 
@@ -54,16 +66,23 @@ def source_load(source_file):
 
                     table_name = _line[1]
 
-                    print('CREATE TABLE {}.{} ( {} {}({})'.format(
+                    print('CREATE TABLE {}.{} ({} {}'.format(
                         _line[0],
                         _line[1],
                         _line[2],
-                        _line[3],
-                        _line[4]
+                        sub_dt(_line, ora_dt_map))
                     )
-                    )
+
+#                    print('CREATE TABLE {}.{} ( {} {}({})'.format(
+#                        _line[0],
+#                        _line[1],
+#                        _line[2],
+#                        _line[3],
+#                        _line[4]
+#                    )
+#                    )
                 else:
-                    print(', {} {}({})'.format(_line[2], _line[3], _line[4]))
+                    print(', {} {}'.format(_line[2], sub_dt(_line, ora_dt_map)))
 
     print(');')
 
@@ -79,12 +98,12 @@ def main(sys_params):
     if ora_dt_map > 0:
 
         print("Length: ",len(ora_dt_map))
-        source_load('../inbound/2018-01-08_uscomdv1_tab_col_1.lst')
+        print(ora_dt_map)
+        source_load('../inbound/2018-01-08_uscomdv1_tab_col_1.lst',ora_dt_map)
 
-    #dt_replace("../inbound/2017-12-20_uscomdv1_marketplace_tables.sql", "target_file", ora_dt_map)
-    #dt_replace("../inbound/2018-01-02_uscomdv1_marketplace_tables.sql", "target_file", ora_dt_map)
+        #l = ['MARKETPLACE', 'USER_QUERIES_TIMING_STATS_VW', 'NUMBER_OF_QUERIES', 'NUMBER', '22', '0']
+        #print(sub_dt(l,ora_dt_map))
 
-   # print
 
 # Main #
 if __name__ == "__main__":
