544d0d445d73b8ecf87737b153e7a5dc3022376a
vitkalovav@gmail.com <arrows364574>
2018-01-20 23:03:51 +0000

    2018-01-20 oraconn restructured

--- bin/oracle_exp.py
+++ bin/oracle_exp.py
@@ -1,17 +1,38 @@
 import cx_Oracle
 import psycopg2
 
+
 def ora_conn():
-    conn = cx_Oracle.connect("system", "", "dhemrep_uksrv")
+    print("Oracle connection")
+    ora_conn = cx_Oracle.connect("system", "", "exa_uscomdev1")
+    do_query_ora(ora_conn)
+    ora_conn.close()
+
+def do_query_ora(conn):
     cursor = conn.cursor()
     cursor.execute("""SELECT table_name 
                         FROM dba_tables 
-                        WHERE owner = :iowner""",
-                    iowner = 'XDB')
+                        WHERE owner = :iowner
+                        and table_name = :itable""",
+                    iowner = 'MARKETPLACE',
+                    itable = 'META_DICTIONARY')
     for tname in cursor:
         print("Table_names:", tname)
+        tab_col(conn, tname)
 
-def do_query( conn ) :
+def tab_col(iconn, itable):
+    cur = iconn.cursor()
+    cur.execute("""select column_name 
+                        from dba_tab_cols 
+                        where table_name = :i_table
+                        and owner = :i_owner
+                        and column_id is not null""",
+                    i_table = itable,
+                    i_owner = 'MARKETPLACE')
+    for colname in cur:
+        print(colname)
+
+def do_query_psg( conn ) :
     cur = conn.cursor()
 
     cur.execute( "SELECT * FROM vav_test" )
@@ -23,7 +44,7 @@ def pstg_conn():
     print('Postgres connection')
     myConnection = psycopg2.connect("host='us-ham-svb-2051' port='5459' user='alexeyv' password='iamgroot' dbname='uscomdv1'")
     # port = 5459
-    do_query( myConnection )
+    do_query_psg( myConnection )
     myConnection.close()
 
 ############
